4a921245788e2eeff53f336bd8a27ce73bf4fe8b
Tomáš Procházka <tomas.prochazka@gmail.com>
2018-08-04 21:22:24 +0200

    Fix windows launcher script

--- run-scripts/jmbus-app.bat
+++ run-scripts/jmbus-app.bat
@@ -1,5 +1,5 @@
 ::BATCH file for windows
 set BATDIR=%~dp0
-set LIBDIR=%BATDIR%..\cli-app\build\libs-all
+set LIBDIR=%BATDIR%..\cli-app\build\libs-all\*
 
-java -Djava.ext.dirs=%LIBDIR% org.openmuc.jmbus.app.JmbusApp %*
+java -cp %LIBDIR% org.openmuc.jmbus.app.ConsoleApp %*
